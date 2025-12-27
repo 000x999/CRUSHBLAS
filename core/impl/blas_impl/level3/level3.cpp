4fa719318e7d3a0bead8b774b872331fe81c67c9
000x999 <cxrnxgxz@gmail.com>
2025-12-27 01:27:18 -0500

    Following commits are BLOCK sizing tests for the level3::blas::crush_gemm kernel, BLOCK I = 16, BLOCK J =512, BLOCK K = 2

--- core/impl/blas_impl/level3/level3.cpp
+++ core/impl/blas_impl/level3/level3.cpp
@@ -576,9 +576,9 @@ void level3::blas::crush_gemm(transpose_gemm transpose_left, transpose_gemm tran
   size_t k = (transpose_left  == transpose_gemm::no_transpose) ? left_view.col_view  : left_view.row_view;
   size_t n = (transpose_right == transpose_gemm::no_transpose) ? right_view.col_view : right_view.row_view;
 
-  constexpr size_t BLOCK_I = 32;
+  constexpr size_t BLOCK_I = 16;
   constexpr size_t BLOCK_J = 512;
-  constexpr size_t BLOCK_K = 4;
+  constexpr size_t BLOCK_K = 2;
 
   float* A = left_view.data_view;
   float* B = right_view.data_view;
