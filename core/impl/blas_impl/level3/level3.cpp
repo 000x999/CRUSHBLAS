d8e4f172501dbd9ab36bffa054fe7a8c73e54e09
000x999 <cxrnxgxz@gmail.com>
2025-12-27 01:58:46 -0500

    This is the current optimal block sizes with a 128 wide loop unroll on AVX512, BLOCK I = 32, BLOCK J = 128, BLOCK K = 4

--- core/impl/blas_impl/level3/level3.cpp
+++ core/impl/blas_impl/level3/level3.cpp
@@ -577,7 +577,7 @@ void level3::blas::crush_gemm(transpose_gemm transpose_left, transpose_gemm tran
   size_t n = (transpose_right == transpose_gemm::no_transpose) ? right_view.col_view : right_view.row_view;
 
   constexpr size_t BLOCK_I = 32;
-  constexpr size_t BLOCK_J = 256;
+  constexpr size_t BLOCK_J = 128;
   constexpr size_t BLOCK_K = 4;
 
   float* A = left_view.data_view;
