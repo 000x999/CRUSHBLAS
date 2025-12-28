775311e015849c73498382318de63ba6ef74bd9d
000x999 <cxrnxgxz@gmail.com>
2025-12-27 21:15:34 -0500

    Testing out pre-fetching positioning and prefetch sizes/iterations

--- core/impl/blas_impl/level3/level3.cpp
+++ core/impl/blas_impl/level3/level3.cpp
@@ -609,12 +609,12 @@ void level3::blas::crush_gemm(transpose_gemm transpose_left, transpose_gemm tran
         size_t k_end = std::min(k_block + BLOCK_K, k);
 
         for (size_t i = i_block; i < i_end; ++i) {
-          _mm_prefetch(&A[(i + 1) * lda + k_block], _MM_HINT_T0);
+          _mm_prefetch(&A[(i + 8) * lda + k_block], _MM_HINT_T0);
 
           for (size_t kk = k_block; kk < k_end; ++kk) {
             if(kk + 1 < k_end){
-              _mm_prefetch(&B[(kk + 1) * ldb + j_block], _MM_HINT_T0); 
-              _mm_prefetch(&B[(kk + 1) * ldb + j_block + 64], _MM_HINT_T0);
+              _mm_prefetch(&B[(kk + 8) * ldb + j_block], _MM_HINT_T0); 
+              _mm_prefetch(&B[(kk + 8) * ldb + j_block + 64], _MM_HINT_T0);
             }
             float a_val = A[i * lda + kk];
             __m512 a_vec = _mm512_set1_ps(a_val * alpha);
@@ -622,8 +622,8 @@ void level3::blas::crush_gemm(transpose_gemm transpose_left, transpose_gemm tran
             size_t j = j_block;
               
             for (; j + 127 < j_end; j += 128) {
-              _mm_prefetch(&B[(kk + 1) * ldb + j_block], _MM_HINT_T0); 
-              _mm_prefetch(&B[(kk + 1) * ldb + j_block + 64], _MM_HINT_T0);
+             // _mm_prefetch(&B[(kk + 1) * ldb + j_block], _MM_HINT_T0); 
+             // _mm_prefetch(&B[(kk + 1) * ldb + j_block + 64], _MM_HINT_T0);
              
               _mm_prefetch(&B[kk * ldb + j + 256], _MM_HINT_T0);
               _mm_prefetch(&c_buffer[i + 1 - i_block][j - j_block], _MM_HINT_T0);
